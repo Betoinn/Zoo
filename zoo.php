309c247c108a316585996049516223a02f0fb701
Betoinn <nico.betoin@gmail.com>
2024-11-08 09:21:36 +0100

    Update zoo.php

--- zoo.php
+++ zoo.php
@@ -1,7 +1,12 @@
 <?php
 
+$server = "localhost";
+$username = "root";
+$password = "";
+$databaseName = "inscription";
 
-
+//Connexion à la base de donnée
+$conn = new mysqli($server, $username, $password, $databaseName);
 
 
 
