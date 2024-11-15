e44a2e7688cd64844a56501991ae2989df64e8c6
Betoinn <nico.betoin@gmail.com>
2024-11-15 11:49:50 +0100

    Update avis.php

--- avis.php
+++ avis.php
@@ -11,6 +11,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $requete->bind_param("iiis", $enclosId, $utilisateurId, $note, $commentaire);
     $requete->execute();
 
-    echo "Avis ajouté avec succès !";
+    echo "Votre avis a bien été pris en compte";
 }
 ?>
