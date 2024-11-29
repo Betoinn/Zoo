2ed6b27ddd8abf11ee7f0bb74b2ef829a3639a8d
Betoinn <nico.betoin@gmail.com>
2024-11-29 08:50:06 +0100

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -5,10 +5,14 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $nomUtilisateur = $_POST["nom_utilisateur"];
     $motDePasse = password_hash($_POST["mot_de_passe"], PASSWORD_DEFAULT);
 
+    // Préparation de la requête d'insertion
     $requete = $connexion->prepare("INSERT INTO utilisateurs (nom_utilisateur, mot_de_passe, role) VALUES (?, ?, 'utilisateur')");
     $requete->bind_param("ss", $nomUtilisateur, $motDePasse);
-    $requete->execute();
 
-    echo "Inscription réussie";
+    if ($requete->execute()) {
+        echo "Inscription réussie";
+    } else {
+        echo "Erreur lors de l'inscription. Veuillez réessayer.";
+    }
 }
 ?>
