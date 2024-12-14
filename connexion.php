e453a5e35abd6db9549b41595f61ce846a35ec4d
Betoinn <nico.betoin@gmail.com>
2024-12-14 21:10:47 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -1,6 +1,11 @@
 <?php
 include 'config.php';
 
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     // Récupération des données du formulaire
     $nomUtilisateur = htmlspecialchars(trim($_POST['nom_utilisateur']));
