04e3a3815e181319af3a7a4848800cef5732f7ea
Betoinn <nico.betoin@gmail.com>
2024-12-16 10:27:55 +0100

    Update biomes.php

--- biomes.php
+++ biomes.php
@@ -1,17 +1,35 @@
 <?php
 include 'config.php';
 
-// Récupérer tous les biomes
-$requete = $connexion->prepare("SELECT id, nom_biome FROM biomes");
-$requete->execute();
-$resultat = $requete->get_result();
+// Requête pour récupérer les biomes et leurs enclos
+$queryBiomes = "
+    SELECT biomes.id AS biome_id, biomes.nom_biome, enclos.id AS enclos_id, enclos.nom_enclos 
+    FROM biomes 
+    LEFT JOIN enclos ON biomes.id = enclos.id_biome 
+    ORDER BY biomes.id, enclos.id
+";
+
+$result = $connexion->query($queryBiomes);
 
 $biomes = [];
-while ($biome = $resultat->fetch_assoc()) {
-    $biomes[] = $biome;
+while ($row = $result->fetch_assoc()) {
+    $biomeId = $row['biome_id'];
+    if (!isset($biomes[$biomeId])) {
+        $biomes[$biomeId] = [
+            'nom_biome' => $row['nom_biome'],
+            'enclos' => []
+        ];
+    }
+    if ($row['enclos_id']) {
+        $biomes[$biomeId]['enclos'][] = [
+            'id' => $row['enclos_id'],
+            'nom_enclos' => $row['nom_enclos']
+        ];
+    }
 }
 
 // Retourner les données au format JSON
 header('Content-Type: application/json');
 echo json_encode($biomes);
+
 ?>
