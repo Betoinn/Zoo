fd380121588d5aebf0c38972a12f3de0dee1f066
Betoinn <nico.betoin@gmail.com>
2024-12-16 10:49:51 +0100

    Update biomes.php

--- biomes.php
+++ biomes.php
@@ -1,35 +1,28 @@
 <?php
 include 'config.php';
 
-// Requête pour récupérer les biomes et leurs enclos
-$queryBiomes = "
-    SELECT biomes.id AS biome_id, biomes.nom_biome, enclos.id AS enclos_id, enclos.nom_enclos 
-    FROM biomes 
-    LEFT JOIN enclos ON biomes.id = enclos.id_biome 
-    ORDER BY biomes.id, enclos.id
-";
+// Requête pour récupérer les biomes et les enclos associés
+$query = "SELECT biomes.id AS biome_id, biomes.nom_biome, enclos.id AS enclos_id, enclos.nom_enclos
+          FROM biomes
+          LEFT JOIN enclos ON biomes.id = enclos.id_biome
+          ORDER BY biomes.id, enclos.id";
+$result = $connexion->query($query);
 
-$result = $connexion->query($queryBiomes);
-
+// Organiser les données
 $biomes = [];
 while ($row = $result->fetch_assoc()) {
-    $biomeId = $row['biome_id'];
-    if (!isset($biomes[$biomeId])) {
-        $biomes[$biomeId] = [
-            'nom_biome' => $row['nom_biome'],
-            'enclos' => []
-        ];
-    }
-    if ($row['enclos_id']) {
-        $biomes[$biomeId]['enclos'][] = [
-            'id' => $row['enclos_id'],
-            'nom_enclos' => $row['nom_enclos']
-        ];
-    }
+    $biomes[$row['biome_id']]['nom_biome'] = $row['nom_biome'];
+    $biomes[$row['biome_id']]['enclos'][] = [
+        'id' => $row['enclos_id'],
+        'nom_enclos' => $row['nom_enclos']
+    ];
 }
 
-// Retourner les données au format JSON
-header('Content-Type: application/json');
-echo json_encode($biomes);
-
+// Générer la page HTML avec les biomes et les enclos
+foreach ($biomes as $biome) {
+    echo "<h2>" . $biome['nom_biome'] . "</h2>";
+    foreach ($biome['enclos'] as $enclos) {
+        echo "<a href='enclos.php?id_enclos=" . $enclos['id'] . "'>Enclos " . $enclos['nom_enclos'] . "</a><br>";
+    }
+}
 ?>
